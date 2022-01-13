283050df4d7f785d2ca2e47b07eeffd7eb0d97ce
Petia Kortcheva <peetiak@gmail.com>
2022-01-13 11:28:58 -0800

    Add statements for results

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-
 $(document).ready(function() {
   $("form#dating-site").submit(function(event) {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
+    $("#BradPitt").hide();
+    $("#TomHanks").hide();
+    $("#SandraBullock").hide();
+    console.log(age);
+    console.log(gender);
+
+    if (gender === 'female' && age > 21 && age <= 30) {
+      $("#BradPitt").show();
+     } else if (gender === 'female' && age > 31) {
+       $("#TomHanks").show();
+     } else {
+       $("#SandraBullock").show();
+     }
 
     event.preventDefault();
   });
